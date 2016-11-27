c1aec29b25dcd9a2d1b4d1b587b7c6417a33c348
Álvaro Guimarães <alvaroguimaraes@gmail.com>
2016-11-27 23:38:16 +0100

    First commit.

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception  $e
+     * @return void
+     */
+    public function report(Exception $e)
+    {
+        parent::report($e);
+    }
+
+    /**
+     * Render the exception
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param Exception $e
+     *
+     * @return Response
+     */
+    public function render($request, Exception $e)
+    {
+        $code = 500;
+        if ($e instanceof HttpException) {
+            $code = $e->getStatusCode();
+        }
+
+        return new Response(null, $code);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+$app->post('register', function () {
+    return 'oi';
+});
+
+$app->post('unregister', function () {
+    return 'bye';
+});
